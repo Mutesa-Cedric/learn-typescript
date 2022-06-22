4f201ea8726df5121b1f3a71a254df1eaace5f36
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-22 08:54:01 +0200

    update : using generics with interfaces

--- typeScript Fundamentals/14.generics/generics.ts
+++ typeScript Fundamentals/14.generics/generics.ts
@@ -8,12 +8,10 @@ const returnLast=<T>(arr:Array<T>):T=>{
 
 const l1= returnLast(["m","u","t","e","s","a"]);
 //l1 automatically get the type of string;
-
 const l2= returnLast([1,2,3,4,5,6,7,8,9,10]);
 //l2 automatically get the the number type.
 
 //typescript does inference but you can specify the type your self like:
-
 const l3=<number> returnLast([1,2,3,4,5,]);
 
 //2.make array function which will make the array of values passed in based on their type
@@ -21,7 +19,6 @@ const l3=<number> returnLast([1,2,3,4,5,]);
 const makeArray =<X,Y>(x:X,y:Y):[X,Y]=>{
     return [x,y];
 }
-
 const nameArray=makeArray("mutesa","cedric");//stringArray[X,Y]
 const numberArray=makeArray(2,4);//numberArray[X,Y]
 const mixArray=makeArray("mutesa",1);//mixArray[X,Y]
@@ -39,9 +36,12 @@ const person2=makeFullName({firstName:"gihozo",lastName:"germaine",randomField:"
 // const person3=makeFullName({lastName:"random",another:"random"})//this will not work because we have defined that lastname and firstname must be available
 console.log(person2.randomField);//this will work because we have kept all fields .
 
+//using generics with interfaces
 
-
-
-
-
-
+interface Tab<T>{
+    id:string
+    postion:string
+    data:T
+}
+type  NumberTab=Tab<number>;
+type StringTab=Tab<string>;
